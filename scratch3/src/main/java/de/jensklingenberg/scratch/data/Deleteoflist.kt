c39cede1dd94743f4e457940ad3319be518d9775
Jens Klingenberg <mail@jensklingenberg.de>
2024-11-01 20:21:05 +0100

    Deleteoflist

--- scratch3/src/main/java/de/jensklingenberg/scratch/data/Deleteoflist.kt
+++ scratch3/src/main/java/de/jensklingenberg/scratch/data/Deleteoflist.kt
@@ -1,19 +1,13 @@
-package de.jensklingenberg.scratch.looks
-
 import de.jensklingenberg.scrako.builder.ScriptBuilder
 import de.jensklingenberg.scrako.common.BlockSpec
 import de.jensklingenberg.scrako.common.Context
 import de.jensklingenberg.scrako.common.Node
 import de.jensklingenberg.scrako.common.ReporterBlock
-import de.jensklingenberg.scrako.common.StackBlock
 import de.jensklingenberg.scrako.common.setValue
 import de.jensklingenberg.scrako.model.Block
 import java.util.UUID
 
-/**
- * https://en.scratch-wiki.info/wiki/Think_()_(block)
- */
-private class Think(val block0 : ReporterBlock, ) : Node, StackBlock {
+private class Deleteoflist(val block0: ReporterBlock, val list: String) : Node {
     override fun visit(
         visitors: MutableMap<String, Block>,
         parent: String?,
@@ -23,16 +17,16 @@ private class Think(val block0 : ReporterBlock, ) : Node, StackBlock {
     ) {
         val block0Id = UUID.randomUUID().toString()
         visitors[identifier] = BlockSpec(
-            opcode = "looks_think",
+            opcode = "data_deleteoflist",
             inputs = mapOf(
-                "MESSAGE" to setValue(block0, block0Id, context) 
+                "INDEX" to setValue(block0, block0Id, context)
             ),
             fields = mapOf(
-                
+                "LIST" to listOf(list, null)
             )
         ).toBlock(nextUUID, parent)
         block0.visit(visitors, identifier, block0Id, null, context)
     }
 }
 
-fun ScriptBuilder.think(block0: ReporterBlock) = addNode(Think(block0))
+fun ScriptBuilder.deleteItemOfList(block0: ReporterBlock, list: String) = addNode(Deleteoflist(block0, list))
